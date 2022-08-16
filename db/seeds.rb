df164b8ef39fe2903e47ea47ffd79bc1efdc4061
philippine von rochow <philippine.vonrochow@sciencespo.fr>
2022-08-16 16:20:05 +0200

    article

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+10.times do
+  Article.create([{
+   title: Faker::Book.title,
+   content: Faker::Book.author
+  }])
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
